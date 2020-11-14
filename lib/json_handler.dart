128b20faf910f1e7559d634fd678662a18c32f8a
Atri Das <babinsamrat123@gmail.com>
2020-11-14 13:49:22 +0530

    handles error and add headers

--- lib/json_handler.dart
+++ lib/json_handler.dart
@@ -8,20 +8,49 @@ class JSONHandler {
 
   final data = 'API is not invoked';
 
-  Future getData({String url}) async {
-    if(url == null){
+  ///[*] the [getData] method get Data from an API
+  ///[*] if fails to get data it will throw an [error]
+  ///[*] this method must needs an [url] and it also accepts [header]
+
+  Future getData({String url, Map header}) async {
+    //checks the provided url is null or not
+
+    if (url == null) {
       return 'URL can not be null';
-    }else{
-      _response = await _dio.get(url);
-      try {
-        _response = await _dio.get(url);
-        if (_response.statusCode == 200) {
-          return _response.data;
-        } else {
-          return 'error occurred';
+    } else {
+      if (header != null) {
+        try {
+          _dio.options.headers = Map<String, dynamic>.from(header);
+          _response = await _dio.get(url);
+          print(_response.statusCode.toString());
+          if (_response.statusCode == 200) {
+            return _response.data;
+          } else {
+            return 'error occurred';
+          }
+        } on DioError catch (error) {
+          if (error.type == DioErrorType.CONNECT_TIMEOUT) {
+            return 'Connection Time Out';
+          } else {
+            return 'Unexpected error occurred';
+          }
         }
-      } catch (e) {
-        return 'unexpected error occurred';
+      } else {
+        _response = await _dio.get(url);
+        try {
+          _response = await _dio.get(url);
+          if (_response.statusCode == 200) {
+            return _response.data;
+          } else {
+            return 'error occurred';
+          }
+        } on DioError catch (error) {
+          if (error.type == DioErrorType.CONNECT_TIMEOUT) {
+            return 'Connection Time Out';
+          } else {
+            return 'Unexpected error occurred';
+          }
+        }
       }
     }
   }
